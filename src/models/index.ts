03ec86dc16e1b3b2ca0c2945266b66038a87ac0a
Azaken1248 <suvkrm@gmail.com>
2026-05-17 23:38:24 +0530

    add main export file

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export { GlobalConfig, type IGlobalConfigDocument } from './GlobalConfig.js';
+export { Artwork, type IArtworkDocument } from './Artwork.js';
+export { CommissionTier, type ICommissionTierDocument } from './CommissionTier.js';
+export { FaqItem, type IFaqItemDocument } from './FaqItem.js';
+export { TosSection, type ITosSectionDocument, type TosVariant } from './TosSection.js';
